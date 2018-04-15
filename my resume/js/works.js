3892d52e4ccc923c1329d902eb7bdfd657aee95d
Tream0312 <743479880@qq.com>
2018-04-15 17:33:22 +0800

    Signed-off-by: Tream0312 <743479880@qq.com>

--- my resume/js/works.js
+++ my resume/js/works.js
@@ -3,38 +3,32 @@
         const worksLen = works.children().length; 
         const clasName = 'ui-page ui-body-c ui-page-active';         
         //当图片1滑动时
-        $(document).on('pageinit','#img1',function(){
-          $('div').on('swipeleft',function(){
+        $(document).on('pageinit','div',function(){
+          $('#works div').on('swipeleft',function(){
             //先隐藏所有图片
-            console.log(worksLen);
-            console.log('left'); 
-             $(this).removeClass();
-            $(this).parent().next().addClass(clasName);           
+           // console.log(worksLen);
+            //console.log('left'); 
+             $('#works').children().removeClass();
+            $(this).next('div').addClass(clasName);           
             if($(this).index()>worksLen-2){
-              works.children('div').removeClass(clasName);
-              works.children('#img1').addClass(clasName);                     
-            }
-            //console.log($(this).index());
-             $(this).attr('tabindex',$(this).index());                   
+              //works.children('div').removeClass(clasName);
+              $('#works').children().removeClass();
+              $('#img1').addClass(clasName);                     
+            }            
           })
 
-          $('div').on('swiperight',function(){
+          $('#works div').on('swiperight',function(){
             //先隐藏所有图片
-            console.log($(this).index());
+            //console.log($(this).index());
            // alert("您向右滑动！");
-             $(this).removeClass();
-            $(this).parent().prev().addClass('ui-page ui-body-c ui-page-active');
+             $('#works').children().removeClass();
+            $(this).prev('div').addClass('ui-page ui-body-c ui-page-active');
             let attr = $(this).attr('tabindex');
-            //alert(attr);
-            if(attr<=1){
-              works.children('div').removeClass(clasName);
-              works.children('#img4').addClass(clasName);
-             
-            }
-             if(attr=4){
-              $(this).removeClass();
-              $(this).parent().prev().addClass('ui-page ui-body-c ui-page-active');              
-            }          
+            if(attr==1||attr==0){
+             // works.children('div').removeClass(clasName);
+             $('#works').children().removeClass();
+              $('#img4').addClass(clasName);            
+            }      
           })
 
         })
